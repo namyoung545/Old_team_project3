98908c317e8616be2058eb3b307d946c61d51b48
dongyoung <dyk811@gmail.com>
2025-01-09 09:11:36 +0900

    20250709

---
+++ src/main/resources/static/js/dy_calendar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('dy_calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        locale: 'ko',
+        events: jsonData, // JSON 데이터 (Controller에서 전달)
+    });
+    calendar.render();
+});
