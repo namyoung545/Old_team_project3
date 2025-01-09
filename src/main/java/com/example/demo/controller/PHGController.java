a2b82b9ce0307a782053001c794061ce4aacbc1a
CodeLibrary <115143501+executer10@users.noreply.github.com>
2025-01-09 11:14:20 +0900

    update 3file

--- src/main/java/com/example/demo/controller/PHGController.java
+++ src/main/java/com/example/demo/controller/PHGController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PHGController {
     
-    @GetMapping("/index")
+    @GetMapping("/PHG_index")
     public String getIndex() {
         return "/PHG/PHG_index";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/PHG_login")
     public String getLogin() {
         return "/PHG/PHG_login";
     }
 
-    @GetMapping("/join")
+    @GetMapping("/PHG_join")
     public String getSingUp() {
         return "/PHG/PHG_join";
     }
