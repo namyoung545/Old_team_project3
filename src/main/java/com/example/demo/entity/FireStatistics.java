36f07f0fe25620e373755d0dd9fb0cbb83934821
gr2nade <gr2nade@gmail.com>
2025-01-14 10:54:02 +0900

    20250114 Fire Statistics Table

--- src/main/java/com/example/demo/entity/FireStatistics.java
+++ src/main/java/com/example/demo/entity/FireStatistics.java
@@ -8,16 +8,51 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="fire_statistics")
+@Table(name = "fire_statistics")
 public class FireStatistics {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable =false)
+    @Column(nullable = false)
     private Integer year;
 
-    // @Column(nullable = false)
-    // private String 
+    @Column(nullable = false)
+    private String statName;
+
+    @Column(nullable = false)
+    private String statValue;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getStatName() {
+        return statName;
+    }
+
+    public void setStatName(String statName) {
+        this.statName = statName;
+    }
+
+    public String getStatValue() {
+        return statValue;
+    }
+
+    public void setStatValue(String statValue) {
+        this.statValue = statValue;
+    }
     
 }
--- src/main/java/com/example/demo/repository/FireStatisticsRepository.java
+++ src/main/java/com/example/demo/repository/FireStatisticsRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public interface FireStatisticsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.FireStatistics;
+
+@Repository
+public interface FireStatisticsRepository extends JpaRepository <FireStatistics, Integer>{
     
 }
