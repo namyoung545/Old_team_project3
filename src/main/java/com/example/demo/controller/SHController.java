654a4f67ac1e3cf58a38246571074e6e7778c905
gr2nade <gr2nade@gmail.com>
2025-01-16 17:40:15 +0900

    Map Controller

--- src/main/java/com/example/demo/controller/SHController.java
+++ src/main/java/com/example/demo/controller/SHController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class SHController {
@@ -10,6 +12,11 @@ public class SHController {
     public String getSHIndex(Model model) {
         model.addAttribute("message", "안녕하세요! 이것은 Thymeleaf 테스트 페이지입니다.");
         return "/sh_html/sh_index";
+    }
+    
+    @GetMapping("/sh_map")
+    public String getSHMap(Model model) {
+        return "/sh_html/sh_map";
     }
     
 }
